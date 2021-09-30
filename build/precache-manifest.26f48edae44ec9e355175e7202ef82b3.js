8b397ccac6ed77297da97c134303365b5afe69b9
samman06 <monem7242@gmail.com>
2021-09-30 17:46:03 +0200

    add docker and travis file

---
+++ build/precache-manifest.26f48edae44ec9e355175e7202ef82b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a4b355fd41a8296608739d856499df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b294ae66eb5a3d550ca7",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "8f5a27497a017cc13074",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b294ae66eb5a3d550ca7",
+    "url": "/static/js/2.f43ffcf6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.f43ffcf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f5a27497a017cc13074",
+    "url": "/static/js/main.ce17295e.chunk.js"
+  },
+  {
+    "revision": "cc113234ba27267959cf",
+    "url": "/static/js/runtime-main.b5a572b5.js"
+  }
+]);
